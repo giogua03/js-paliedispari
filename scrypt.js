9bf2ec5a1a29fc0594b3ba7b5bb89ff0fa37683c
giogua03 <giorgiogua03@gmail.com>
2022-10-28 15:18:26 +0200

    parola

---
+++ scrypt.js
@@ -0,0 +1,21 @@
+//Palindroma
+//Chiedere all’utente di inserire una parola
+//Creare una funzione per capire se la parola inserita è palindroma
+
+
+///pati o dispati
+//L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+//Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+//Sommiamo i due numeri
+//Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+//Dichiariamo chi ha vinto.
+const userParola = prompt('dammi una parola');
+const arrParola= [""];
+arrParola.push = userParola
+if (arrParola.reverse = userParola){
+    alert('Parola Palindroma');
+} else {
+    alert('Parola NON Palindroma');
+}
+
+
